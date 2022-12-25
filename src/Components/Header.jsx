d0739053ae5778a8b1ae625427a8a496954a0dfc
ahmet cetin <ahmetlaw@outlook.com>
2022-12-26 00:26:39 +0300

    created components

---
+++ src/Components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="header">
+      <img
+        src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
+        alt="Troll Face"
+      />
+
+      <h2 className="header--title">Meme Generator </h2>
+      <h4 className="header--project">React Course - Project 3</h4>
+    </header>
+  );
+}
+
+export default Header;
