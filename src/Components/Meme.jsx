1c346c84d5eddb6ad3c38da27c5e75c536a83a51
ahmetcetnn <ahmetlaw@outlook.com>
2023-01-01 23:22:01 +0300

    resimler eklendi

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import memesData from "../memesData";
+import { useState } from "react";
 
 function Meme() {
+  const [memeImage, setMemeImage] = useState("")
+  
+    
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+        
+    }
+
+
+
   return (
     <main>
       <div className="form">
         <input className="input" type="text" placeholder="Top text" />
         <input className="input" type="text" placeholder="Bottom text" />
-        <button className="button">Get a new meme image</button>
+        <button className="button" onClick={getMemeImage} >Get a new meme image</button>
       </div>
+      <img src={memeImage} className="memeimage"/>
     </main>
   );
 }
