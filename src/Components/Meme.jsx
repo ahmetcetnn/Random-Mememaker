5a23165630c41dae6fd735f11e213d841b711dda
ahmetcetnn <ahmetlaw@outlook.com>
2023-01-06 20:48:31 +0300

    .

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -3,6 +3,17 @@ import memesData from "../memesData";
 import { useState } from "react";
 
 function Meme() {
+
+
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg" 
+})
+
+
+
+
   const [memeImage, setMemeImage] = useState("")
   
     
@@ -13,16 +24,44 @@ function Meme() {
         
     }
 
+    function handleChange(event){
+      const {name,value} =event.target
+      setMeme(prevMeme => ({
+        ...prevMeme,
+        [name]: value
+    }))
 
+
+    }
 
   return (
     <main>
       <div className="form">
-        <input className="input" type="text" placeholder="Top text" />
-        <input className="input" type="text" placeholder="Bottom text" />
+        <input 
+        className="input" 
+        type="text"
+        placeholder="Top text" 
+        name="topText"
+        value={meme.topText}
+        onChange={handleChange}
+
+        />
+       
+        <input 
+        className="input" 
+        type="text"
+        placeholder="Bottom text" 
+        name="bottomText"
+        value={meme.bottomText}
+        onChange={handleChange}
+        />
+
+
         <button className="button" onClick={getMemeImage} >Get a new meme image</button>
       </div>
       <img src={memeImage} className="memeimage"/>
+      <h2 className="meme--text top">{meme.topText}</h2>
+       <h2 className="meme--text bottom">{meme.bottomText}</h2>
     </main>
   );
 }
